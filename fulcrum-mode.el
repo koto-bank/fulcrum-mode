d2d3f9a2924a398eb85f424c399ba12109c96894
Lämppi Lütti <LamppiLutti@gmail.com>
2023-07-21 12:09:05 +0300

    Init commit

---
+++ fulcrum-mode.el
@@ -0,0 +1,38 @@
+;; fulcrum-mode.el --- Major mode for Fulcrum code -*- lexical-binding: t; -*-
+
+;; Copyright © 2023 Lämppi Lütti <lamppilutti@gmail.com>
+
+;; Authors: Lämppi Lütti <lamppilutti@gmail.com>
+;; Maintainer: Lämppi Lütti <lamppilutti@gmail.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "26.3"))
+;; Keywords: languages
+;; URL: http://github.com/koto-bank/fulcrum-mode
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+
+(provide 'fulcrum-mode)
+
+;;; fulcrum-mode.el ends here.
